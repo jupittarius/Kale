34379ec0b28961bd82f44429ec806a4a9dc83ecd
jupitta <presdell@outlook.com>
2022-08-19 21:17:10 +0800

    driver code

--- toy.cpp
+++ toy.cpp
@@ -364,6 +364,80 @@ static std::unique_ptr<FunctionAST> ParseTopLevelExpr(){
     }
     return nullptr;
 }
+
+// -----------------------------------------
+// |---------Top-Level Parsing-------------|
+// -----------------------------------------
+
+static void HandleDefinition(){
+    if (ParseDefinition()){
+        fprintf(stderr, "Parsed a function definition.\n");
+    } else {
+        // Skip token for error recovery.
+        getNextToken();
+    }
+}
+
+static void HandleExtern() {
+  if (ParseExtern()) {
+    fprintf(stderr, "Parsed an extern\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
+}
+
+static void HandleTopLevelExpression() {
+  // Evaluate a top-level expression into an anonymous function.
+  if (ParseTopLevelExpr()) {
+    fprintf(stderr, "Parsed a top-level expr\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
+}
+
+/// top ::= definition | external | expression | ';'
+static void MainLoop(){
+    while(1){
+        fprintf(stderr, "ready> ");
+        switch (CurTok)
+        {
+        case tok_eof:
+            return;
+        case ';': // ignore top-level semicolons.
+            getNextToken();
+            break;
+        case tok_def:
+            HandleDefinition();
+            break;
+        case tok_extern:
+            HandleExtern();
+            break;
+        default:
+            HandleTopLevelExpression();
+            break;
+        }
+    }
+}
+
+// -----------------------------------------
+// |---------Main driver code.-------------|
+// -----------------------------------------
 int main(){
+    // Install standard binary operators.
+    // 1 is lowest precedence.
+    BinopPrecedence['<'] = 10;
+    BinopPrecedence['+'] = 20;
+    BinopPrecedence['-'] = 20;
+    BinopPrecedence['*'] = 40; // highest.
+
+    // Prime the first token.
+    fprintf(stderr, "ready> ");
+    getNextToken();
+
+    // Run the main "interpreter loop" now.
+    MainLoop();
+    
     return 0;
 }
